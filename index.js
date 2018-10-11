9394313373d0b219b26df0c32222bf925d159afa
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-11 14:59:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,13 +16,20 @@ function logWhisper (string){
 
 function sayHiToGrandma(string){
   if (string.toLowerCase === string) {
+    console.log("I can\'t hear you!")
     return "I can\'t hear you!"
   }
   else if (string.toUpperCase === string) {
+    console.log("YES INDEED!")
     return "YES INDEED!"
   }
   else if (string === "I love you, Grandma.") {
+    console.log("I love you, too.")
     return "I love you, too."
   }
   return
 }
+
+sayHiToGrandma("hello")
+sayHiToGrandma("HELLO")
+sayHiToGrandma("I love you, Grandma.")
